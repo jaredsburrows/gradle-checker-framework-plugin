8d33ceef77d45f6e78490a1517d1843849b9ab3b
Jared Burrows <jaredsburrows@gmail.com>
2021-01-02 21:24:30 -0500

    update com.github.ben-manes.versions to 0.36.0

--- src/test/groovy/com/jaredsburrows/checkerframework/CheckerPluginSpec.groovy
+++ src/test/groovy/com/jaredsburrows/checkerframework/CheckerPluginSpec.groovy
@@ -2,19 +2,25 @@ package com.jaredsburrows.checkerframework
 
 import org.gradle.testkit.runner.BuildResult
 import org.gradle.testkit.runner.GradleRunner
+import org.junit.Rule
+import org.junit.rules.TemporaryFolder
+import spock.lang.Specification
 import spock.lang.Unroll
-import test.BaseSpecification
 
-final class CheckerPluginSpec extends BaseSpecification {
-  static final List<String> TESTED_GRADLE_VERSIONS = [
-    '3.4',
-    '3.5',
+final class CheckerPluginSpec extends Specification {
+  @Rule TemporaryFolder testProjectDir = new TemporaryFolder()
+  def buildFile
+  private static final List<String> TESTED_GRADLE_VERSIONS = [
     '4.0',
     '4.5',
     '4.9',
     '4.10',
     '5.0',
   ]
+
+  def "setup"() {
+    buildFile = testProjectDir.newFile("build.gradle")
+  }
 
   @Unroll def "java project running licenseReport using with gradle #gradleVersion"() {
     given:
@@ -46,8 +52,7 @@ final class CheckerPluginSpec extends BaseSpecification {
     gradleVersion << TESTED_GRADLE_VERSIONS
   }
 
-  @Unroll
-  def 'without relevant plugin, compiler settings are not applied using version #gradleVersion'() {
+  @Unroll def 'without relevant plugin, compiler settings are not applied using version #gradleVersion'() {
     given:
     buildFile <<
       """
@@ -76,8 +81,7 @@ final class CheckerPluginSpec extends BaseSpecification {
     gradleVersion << TESTED_GRADLE_VERSIONS
   }
 
-  @Unroll
-  def 'with relevant plugin, compiler settings are applied using version #gradleVersion'() {
+  @Unroll def 'with relevant plugin, compiler settings are applied using version #gradleVersion'() {
     given:
     buildFile <<
       """
@@ -107,5 +111,4 @@ final class CheckerPluginSpec extends BaseSpecification {
     where:
     gradleVersion << TESTED_GRADLE_VERSIONS
   }
-
 }
